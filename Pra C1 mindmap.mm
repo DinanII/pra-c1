dbdf7705052b0c0d0067cca1b0d758344a6ea44a
Dinan <116496595+DinanII@users.noreply.github.com>
2023-09-06 18:38:19 +0200

    Initial commit

---
+++ Pra C1 mindmap.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Pra C1 mindmap" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1694010671597" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Ontwerp / Layout" POSITION="bottom_or_right" ID="ID_816295826" CREATED="1694010678733" MODIFIED="1694010758814">
+<edge COLOR="#ff0000"/>
+<node TEXT="Wireframes" ID="ID_702689423" CREATED="1694010700059" MODIFIED="1694010712312">
+<node TEXT="Miro (Google):&#xa;https://miro.com/app/board/uXjVMnoYpiM=/?share_link_id=167650104769" ID="ID_86885009" CREATED="1694010713788" MODIFIED="1694010747590"/>
+</node>
+</node>
+<node TEXT="Inhoud" POSITION="top_or_left" ID="ID_437279507" CREATED="1694010763142" MODIFIED="1694010774691">
+<edge COLOR="#00ff00"/>
+<node TEXT="Header" ID="ID_612861349" CREATED="1694010895134" MODIFIED="1694010898743">
+<node TEXT="Afbeeldingen" ID="ID_1676713362" CREATED="1694010900644" MODIFIED="1694010907183">
+<node TEXT="Curio Logo" ID="ID_1861052871" CREATED="1694010907779" MODIFIED="1694010912213"/>
+<node TEXT="Nieuw Curio SD Logo" ID="ID_721732669" CREATED="1694010912772" MODIFIED="1694010932926"/>
+<node TEXT="Relevante foto (schoolgebouw, iets software gerelateerd)" ID="ID_1874531374" CREATED="1694010940382" MODIFIED="1694010986774"/>
+</node>
+<node TEXT="Tekst" ID="ID_1713725979" CREATED="1694010996212" MODIFIED="1694010998822">
+<node TEXT="" ID="ID_878999481" CREATED="1694011000940" MODIFIED="1694011000940"/>
+</node>
+</node>
+<node TEXT="Main" ID="ID_581943031" CREATED="1694011249795" MODIFIED="1694011254391">
+<node TEXT="Tekst" ID="ID_348679660" CREATED="1694011257981" MODIFIED="1694011262198">
+<node TEXT="Wat is Curio" ID="ID_1017787375" CREATED="1694011264445" MODIFIED="1694011271471"/>
+</node>
+</node>
+</node>
+</node>
+</map>
